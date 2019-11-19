a0a5351adcac4082d7635bc923d09eaa9a8f9653
Airat71 <az@v-exp.ru>
2019-11-19 19:19:26 +0300

    added summa.c

---
+++ summa.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Введите число a:\n);
+    scanf("%d", &a);
+    printf("Введите число b:\n);
+    scanf("%d", &b);
+    printf("Их сумма равна %d\n", a + b);
+    return 0;
+}
